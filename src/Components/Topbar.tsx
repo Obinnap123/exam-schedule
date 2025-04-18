dda9ea1eda1f40c3468615a0fb081108563f6b02
Obinnap123 <paulobinna493@gmail.com>
2025-04-18 18:22:18 +0100

    just added components

--- src/Components/Topbar.tsx
+++ src/Components/Topbar.tsx
@@ -3,8 +3,6 @@ import React from "react";
 function Topbar() {
   return (
     <>
-      <div>the name of a boy</div>
-
       <div className="h-16 bg-white shadow-md px-6 flex items-center justify-between mt-[30px] rounded-[5px]">
         <h3 className="text-xl font-semibold text-black">Dashboard</h3>
         <div className="flex items-center space-x-4">
